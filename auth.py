c59b9cbab7d60d3ff8cecf140a0bbf29de318fef
rn7s2 <mail@rn7s2.cn>
2022-04-05 11:22:35 +0800

    Auth.

---
+++ auth.py
@@ -0,0 +1,18 @@
+import os
+import json
+from helium import *
+from pathlib import Path
+
+start_firefox('https://auth.bupt.edu.cn/authserver/login')
+
+wait_until(Button('账号登录').exists)
+
+click(S('#username'))
+write(os.getenv('USERNAME'))
+click(S('#password'))
+write(os.getenv('PASSWORD'))
+click(Button('账号登录'))
+
+Path('cookies.txt').write_text(json.dumps(get_driver().get_cookies()))
+
+kill_browser()
